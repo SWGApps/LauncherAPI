ef922b99df77d50b386e0fe0d0a206cde46cd30f
Zac Jones <zjmw00@gmail.com>
2021-05-22 23:12:28 -0500

    [Updated] Default DB secret, was missing a '!'

--- Models/Properties/DatabaseProperties.cs
+++ Models/Properties/DatabaseProperties.cs
@@ -2,7 +2,7 @@ namespace LauncherAPI.Models.Properties
 {
     public static class DatabaseProperties
     {
-        public static string DBSecret = "swgemus3cr37";
+        public static string DBSecret = "swgemus3cr37!";
         public static string DBHost = "localhost";
         public static int DBPort = 3306;
         public static string DBUser = "swgemu";
