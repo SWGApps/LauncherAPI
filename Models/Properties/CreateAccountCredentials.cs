db50d43992709e59ae0041676d89d95b746f0c8e
1sudo <zjmw00@gmail.com>
2021-05-27 02:24:55 -0500

    [Updated] Account Creation Property

--- Models/Properties/CreateAccountCredentials.cs
+++ Models/Properties/CreateAccountCredentials.cs
@@ -5,6 +5,8 @@ namespace LauncherAPI.Models.Properties
         public string Password { get; set; }
         public string PasswordConfirmation { get; set; }
         public string Email { get; set; }
-        public string Captcha { get; set; }
+        public string CaptchaValue1 { get; set; }
+        public string CaptchaValue2 { get; set; }
+        public string CaptchaAnswer { get; set; }
     }
 }
